1decfc0794cbebdddc88a4b74f761466cb220758
unitykkk <unitykkk@126.com>
2017-12-07 08:58:11 +0800

    unitykkk
    整理

--- ResLoad/SpeedTester.cs
+++ ResLoad/SpeedTester.cs
@@ -2,36 +2,28 @@
 
 namespace ResLoad
 {
-	public class LoadSpeedTester
+	public class SpeedTester
 	{
 		private DateTime BeginTime;
 		private string frontStr = string.Empty;
+		private uint TotalBytes = 0;
 
-		public LoadSpeedTester(string frontStr = "")
+		public SpeedTester(string frontStr = "")
 		{
 			this.frontStr = frontStr;
 			this.BeginTime = DateTime.Now;
 		}
-
-		public void CostTime()
-		{
-			ConsoleMgr.LogRed(frontStr + "结束" + " : " + System.DateTime.Now.ToString());
-			TimeSpan span = DateTime.Now - BeginTime;
-			string str = frontStr + "时间" + " : " + span.TotalSeconds + "秒";
-			ConsoleMgr.LogRed(str);
-		}
-
-		private uint totalBytes = 0;
 		public void AddLoaded(uint loadedBytes)
 		{
-			totalBytes += loadedBytes;
+			TotalBytes += loadedBytes;
 		}
 
 		public void CountSpeed()
 		{
 			TimeSpan span = DateTime.Now - BeginTime;
 			double costSeconds = span.TotalSeconds;
-			double totalMB = (double)totalBytes / (1024 * 1024);
+//			ConsoleMgr.LogRed (costSeconds.ToString ());
+			double totalMB = (double)TotalBytes / (1024 * 1024);
 
 			double speed = totalMB / costSeconds;
 			string str = frontStr + speed.ToString("F3") +  "MB/S";
--- ResLoad/Test.cs
+++ ResLoad/Test.cs
@@ -8,25 +8,18 @@ using System.Threading;
 
 namespace ResLoad
 {
-	class MainClass
+	class Test
 	{
-		private static LoadSpeedTester SpeedTester1 = null;
-		private static LoadSpeedTester SpeedTester2 = null;
-		private static LoadSpeedTester SpeedTester3 = null;
+		private static SpeedTester SpeedTester1 = null;
+		private static SpeedTester SpeedTester2 = null;
 
 
 		public static void Main (string[] args)
 		{
 			//方案1：用系统自带API
-			SpeedTester1 = new LoadSpeedTester("系统普通方案: ");
+			SpeedTester1 = new SpeedTester("系统方案: ");
 			LoadFiles ();
 			SpeedTester1.CountSpeed ();
-			Console.WriteLine ("");
-
-//			//方案2，用系统异步加载API
-//			SpeedTester2 = new LoadSpeedTester("系统异步方案-");
-//			LoadFilesAsync();
-
 			Console.WriteLine ("");
 
 			//方案3：用自写API
@@ -57,48 +50,13 @@ namespace ResLoad
 		#endregion
 
 
-		#region 方案2，用系统异步加载API
-		/// <summary>
-		/// 方案2，用系统异步加载API
-		/// </summary>
-		private static void LoadFilesAsync()
-		{
-			string[] filePaths = Directory.GetFiles(GlobalSetting.PackFromFolderPath, "*.*", SearchOption.AllDirectories);
-			for (int i = 0; i < filePaths.Length; i++) 
-			{
-				FileStream fs = new FileStream(filePaths[i], FileMode.Open);
-
-				byte[] datas = new byte[fs.Length];
-				string resName = filePaths[i];
-				IAsyncResult asyncResult = fs.BeginRead(datas, 0, datas.Length,FileAsyncCallback,resName);
-				asyncResult.AsyncWaitHandle.WaitOne();
-				fs.Flush();
-				fs.Close();
-			}
-		}
-
-		private static int AsyncfileCount = 0;
-		public static void FileAsyncCallback (IAsyncResult ar)
-		{
-			string filePath = (string)ar.AsyncState;
-//			ConsoleMgr.LogGreen("系统已异步加载完" + filePath);
-			AsyncfileCount++;
-			if (AsyncfileCount == PackedFileMgr.Ins.PackFilesCount) 
-			{
-				SpeedTester2.CostTime();
-				Console.WriteLine ("");
-			}
-		}
-		#endregion
-
-
-		#region 方案3，用自写API
+		#region 方案2，用自写API
 		private static int SelfFileCount = 0;
 		private static void LoadFilesBySelf()
 		{
 			List<string> packedFileNames = PackedFileMgr.Ins.PackedFileNames;
 
-			SpeedTester3 = new LoadSpeedTester("我的方案: ");
+			SpeedTester2 = new SpeedTester("我的方案: ");
 			for (int i = 0; i < packedFileNames.Count; i++)
 			{
 				string fileName = packedFileNames[i];
@@ -107,7 +65,7 @@ namespace ResLoad
 				if ((returnData != null) && (returnData.Length > 0)) 
 				{
 					SelfFileCount++;
-					SpeedTester3.AddLoaded ((uint)returnData.Length);
+					SpeedTester2.AddLoaded ((uint)returnData.Length);
 				} 
 				else 
 				{
@@ -117,7 +75,7 @@ namespace ResLoad
 
 			if (SelfFileCount == PackedFileMgr.Ins.PackFilesCount) 
 			{
-				SpeedTester3.CountSpeed ();
+				SpeedTester2.CountSpeed ();
 			}
 		}
 		#endregion
